a1eabd0f1c6b4f71752f423fd57433121666a1b8
km-isource <kimsykimanimburu@gmail.com>
2025-06-01 23:50:43 +0300

    loq queries

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -4,19 +4,10 @@ import functools
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        # Extract query from args or kwargs
-        if 'query' in kwargs:
-            sql_query = kwargs['query']
-        elif len(args) > 0:
-            sql_query = args[0]
-        else:
-            sql_query = "Unknown query"
-
-        print(f"Executing SQL Query: {sql_query}")
+        query = kwargs.get('query') or (args[0] if args else None)
+        print(f"Executing query: {query}")
         return func(*args, **kwargs)
-    
     return wrapper
-
 
 @log_queries
 def fetch_all_users(query):
@@ -27,5 +18,5 @@ def fetch_all_users(query):
     conn.close()
     return results
 
-# Fetch users while logging the query
+# Example usage
 users = fetch_all_users(query="SELECT * FROM users")
