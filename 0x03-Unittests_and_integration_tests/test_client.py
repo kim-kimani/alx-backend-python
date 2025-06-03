05f0318be0067f210e1583561a491f3767e7a39a
km-isource <kimsykimanimburu@gmail.com>
2025-06-03 16:47:33 +0300

    Integration tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,7 +27,6 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         cls.get_patcher = patch("client.requests.get")
         cls.mock_get = cls.get_patcher.start()
 
-        # Define side effect to return appropriate JSON based on URL
         def get_json_side_effect(url):
             if url == cls.org_payload["repos_url"]:
                 return cls.repos_payload
@@ -39,20 +38,15 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        """Stop the requests.get patcher."""
         cls.get_patcher.stop()
 
     def test_public_repos(self):
-        """Test public_repos returns expected list of repository names."""
         client = GithubOrgClient(self.org_payload["login"])
         self.assertEqual(client.public_repos(), self.expected_repos)
 
     def test_public_repos_with_license(self):
-        """Test public_repos with license='apache-2.0' returns correct subset."""
         client = GithubOrgClient(self.org_payload["login"])
         self.assertEqual(
             client.public_repos(license="apache-2.0"),
             self.apache2_repos
         )
-        
-        
