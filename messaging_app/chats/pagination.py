2d92f060fbb76ab85b98d37bfe53cf034c7b77b9
km-isource <kimsykimanimburu@gmail.com>
2025-06-07 17:58:25 +0300

    added paginations

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,7 +1,14 @@
 # chats/pagination.py
 from rest_framework.pagination import PageNumberPagination
 
+
 class StandardResultsSetPagination(PageNumberPagination):
+    """
+    A standard pagination class for chat messages/conversations.
+    
+    Uses page-based pagination. Internally uses `page.paginator.count`
+    to determine total number of items.
+    """
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
